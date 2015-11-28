aa53da0a5c70596cd9efbf431f4bb2b8d3c184de
JoshuaHill <jh147@hdm-stuttgart.de>
2015-11-28 13:27:10 +0100

    Classes added
    App, FaceRecog, fileReader., ImageProcessor classes added

---
+++ faceCapture/FaceRecog.java
@@ -0,0 +1,69 @@
+package faceCapture;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.nio.IntBuffer;
+
+import static org.bytedeco.javacpp.opencv_face.*;
+import static org.bytedeco.javacpp.opencv_core.*;
+import static org.bytedeco.javacpp.opencv_imgcodecs.*;
+
+public class FaceRecog {
+	
+	
+
+	public String trainingDir = "src/media/test/";
+	// private FaceRecognizer faceRecognizer = createFisherFaceRecognizer();
+	// private FaceRecognizer faceRecognizer = createEigenFaceRecognizer();
+    private FaceRecognizer faceRecognizer = createLBPHFaceRecognizer();
+
+	
+	
+	// Training
+	public void initFaceRec() {
+		
+        File root = new File(trainingDir);
+
+        FilenameFilter imgFilter = new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                name = name.toLowerCase();
+                return name.endsWith(".jpg") || name.endsWith(".pgm") || name.endsWith(".png");
+            }
+        };
+
+        File[] imageFiles = root.listFiles(imgFilter);
+
+        MatVector images = new MatVector(imageFiles.length);
+
+        Mat labels = new Mat(imageFiles.length, 1, CV_32SC1);
+        
+		@SuppressWarnings("deprecation")
+		IntBuffer labelsBuf = labels.getIntBuffer();
+
+        int counter = 0;
+
+        for (File image : imageFiles) {
+            Mat img = imread(image.getAbsolutePath(), CV_LOAD_IMAGE_GRAYSCALE);
+
+            int label = Integer.parseInt(image.getName().split("\\-")[0]);
+
+            images.put(counter, img);
+
+            labelsBuf.put(counter, label);
+
+            counter++;
+        }
+        faceRecognizer.train(images, labels);
+    }
+	
+	
+	// Recognition
+	public int startRecognition(String path) {
+		
+		int predictedLabel = faceRecognizer.predict(imread(path, CV_LOAD_IMAGE_GRAYSCALE));
+		// return predictedLabel;
+		return predictedLabel;
+	}
+    
+}
+    
---
+++ faceCapture/fileReader.java
@@ -0,0 +1,85 @@
+package faceCapture;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class fileReader {
+	
+	// Reader
+	FileReader fr;
+	BufferedReader br;
+	
+	// Writer
+	BufferedWriter output;
+	
+	// Line Variables
+	String line;
+	String last = null;
+	
+	
+	// Get the Id of the last image
+	public int getImageId() {
+		int imageId = 0;
+		
+		try {
+			fr = new FileReader("src/media/test/test.txt");
+			br = new BufferedReader(fr);
+
+			while ((line = br.readLine()) != null) {
+				// System.out.println(zeile);
+				last = line;
+			}
+		
+			imageId = Integer.parseInt(last.split("\\-")[0]);
+			// System.out.println(imageId);
+		
+			fr.close();
+		}
+		catch (IOException e) {
+			System.out.println("Fehler beim Lesen der Datei ");
+			System.out.println(e.toString());
+		}	
+		return imageId;
+	}
+	
+
+	// Get Name 
+	public String getName(int id) {
+		int tempId = 0;
+		try {
+			fr = new FileReader("src/media/test/test.txt");
+			br = new BufferedReader(fr);
+			
+			while (((line = br.readLine()) != null) && (tempId < id)) {
+				last = line;
+				tempId++;
+			}
+		}
+		catch (IOException e) {
+			System.out.println("Fehler bei Suche");
+			System.out.println(e.toString());
+		}
+		String[] tokens1 = last.split("-");
+		String name = tokens1[1];	
+		return name;
+	}
+	
+	// Add new Entry
+	public void addEntry(String name) {
+		try {
+			output = new BufferedWriter(new FileWriter("src/media/test/test.txt", true));
+			output.newLine();
+			output.append(name);
+			output.close();
+		} 
+		catch (IOException e) {
+			System.out.println("Fehler beim Schreiben in Datei");
+			System.out.println(e.toString());
+		}
+	}
+	
+}
